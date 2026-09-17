0c676602a0a5065b26f6f014dcaf9cfdd7625025
gaoyIntj95 <gaoyintj95@gmail.com>
2026-09-17 09:46:28 +0800

    fix: support router basename on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
 import React from 'react'; import { createRoot } from 'react-dom/client'; import { BrowserRouter } from 'react-router-dom'; import App from './App'; import './styles.css'; import './overview.css'; import './overview-tuning.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter basename={import.meta.env.BASE_URL}><App /></BrowserRouter></React.StrictMode>);
